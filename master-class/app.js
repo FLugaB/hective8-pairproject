19495ab7dcca52322a0453e998c07baae58cebf9
F Luga <fernandus.luga@gmail.com>
2021-11-18 06:32:44 +0700

    Homepage

---
+++ master-class/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.static('public'))
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.render('pages/index');
+  });
+
+app.listen(port, () => {
+  console.log(`Go to ${port}`)
+})
