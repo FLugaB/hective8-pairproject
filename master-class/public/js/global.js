82e11ab29605190cdf790450d8809220ae7837de
F Luga <fernandus.luga@gmail.com>
2021-11-18 15:06:59 +0700

    modal single

--- master-class/public/js/global.js
+++ master-class/public/js/global.js
@@ -10,12 +10,6 @@ function loader_page() {
 
 function showPage() {
   document.getElementById("loader").style.display = "none";
-//   document.getElementById("main").style.display = "block";
-// let low_all = document.getElementsByClassName('low-btn');
-// for (var i = 0; i < low_all.length; i++) {
-//     low_all[i].style.color = '#6366f1 ';
-//     low_all[i].style.background = '#e0e7ff';
-// }
   document.getElementById("animation-zoom-out").style.display = "block";
 }
 
